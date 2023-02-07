7299e8b78808aed44c9a5b4b299eab6f223b1ef9
BLuBin7 <builuongbinh7@gmail.com>
2023-02-07 20:09:41 +0700

    add

---
+++ BTTuan1/Cau1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Cau1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+
+        
+    }
+
+}
